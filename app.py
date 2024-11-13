60e9df0ded01ff153f84f486f3c86412d460dc2a
yorick-1101625 <1101625@hr.nl>
2024-11-13 11:07:32 +0100

    Added render_template and a placeholder html file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import flask
+from flask import render_template
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hallo<p>"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
