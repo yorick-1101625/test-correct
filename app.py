0ee292e7648095197f6034c770927661ff2f4484
Timothy-1031438 <124897596+t3xj@users.noreply.github.com>
2024-11-13 15:08:57 +0100

    [1723] Started connecting the database

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import flask
 from flask import render_template
+from task import Task
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    database = Task()
+    test = database.log_in()
+    return render_template('log-in.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
