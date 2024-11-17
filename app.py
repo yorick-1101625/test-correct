495a546ace75f5e1170794908c0fdd058a9a083a
yorick-1101625 <1101625@hr.nl>
2024-11-17 12:41:23 +0100

    Made prompts from database display on single-question screen inside prompts menu

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 from flask import render_template
 
 from lib.model.questions import Questions
+from lib.model.prompts import Prompts
 
 app = flask.Flask(__name__)
 
@@ -11,9 +12,11 @@ def home():
 
 @app.route('/vraag/<questions_id>', methods=['GET', 'POST'])
 def scoring(questions_id):
-    question_model = Questions()
-    single_question = question_model.read_single_question(questions_id)
-    return render_template('single-question.html', single_question=single_question)
+    questions_model = Questions()
+    single_question = questions_model.read_single_question(questions_id)
+    prompts_model = Prompts()
+    prompts = prompts_model.read_prompts()
+    return render_template('single-question.html', single_question=single_question, prompts=prompts)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- lib/model/prompts.py
+++ lib/model/prompts.py
@@ -1,11 +1,11 @@
 import sqlite3
 from lib.model.database import Database
 
-class Questions:
+class Prompts:
     def __init__(self):
         database = Database('./databases/database.db')
         self.conn, self.cursor = database.connect_db()
 
-    def read_single_question(self, questions_id):
-        result = self.cursor.execute('SELECT * FROM questions WHERE questions_id = ?', (str(questions_id),)).fetchone()
+    def read_prompts(self):
+        result = self.cursor.execute('SELECT * FROM prompts').fetchall()
         return result
