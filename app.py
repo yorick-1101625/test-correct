7563026a0f8df3aba25d6df3e9b56dbdbbf11198
yorick-1101625 <1101625@hr.nl>
2024-11-17 12:05:20 +0100

    Made Questions model with working function to read a single question

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import flask
 from flask import render_template
 
+from lib.model.questions import Questions
+
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -9,7 +11,9 @@ def home():
 
 @app.route('/vraag/<questions_id>', methods=['GET', 'POST'])
 def scoring(questions_id):
-    return render_template('single-question.html', questions_id=questions_id)
+    question_model = Questions()
+    single_question = question_model.read_single_question(questions_id)
+    return render_template('single-question.html', single_question=single_question)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ lib/model/questions.py
@@ -0,0 +1,13 @@
+import sqlite3
+from lib.model.database import Database
+
+class Questions:
+    def __init__(self):
+        database = Database('./databases/database.db')
+        self.conn, self.cursor = database.connect_db()
+
+    def read_single_question(self, questions_id):
+        print(questions_id)
+        result = self.cursor.execute('SELECT * FROM questions WHERE questions_id = ?', (str(questions_id),)).fetchone()
+        print(result)
+        return result
