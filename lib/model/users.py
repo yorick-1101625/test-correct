a8da9d35c320ec315e0e678c08050a785c21fca1
salah <1098082@hr.nl>
2024-12-04 12:36:53 +0100

    edited login html file

--- lib/model/users.py
+++ lib/model/users.py
@@ -8,3 +8,12 @@ class Users():
     def log_in(self):
         result = self.cursor.execute('SELECT * FROM users').fetchall()
         return result
+
+    def log_in(self, username, password):
+        self.cursor.execute('''SELECT login, password FROM users''')
+        credentials = self.cursor.fetchall()
+        for credential in credentials:
+            if username != credential[0] and password != credentials[1]:
+                print('FOUT')
+            else:
+                print('GOED')
