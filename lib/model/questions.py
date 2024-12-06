672d329f7553d793a44f803e9ababdf54b31d771
yorick-1101625 <1101625@hr.nl>
2024-12-06 17:13:00 +0100

    Question Overview: Added Distinct to sql queries, so double questions get filtered out

--- lib/model/questions.py
+++ lib/model/questions.py
@@ -8,19 +8,19 @@ class Questions:
 
     def show_ten_not_indexed_questions(self, offset = 0):
         result = self.cursor.execute(
-            'SELECT * FROM questions WHERE taxonomy_bloom IS NULL OR rtti IS NULL LIMIT ? OFFSET ?',
+            'SELECT DISTINCT * FROM questions WHERE taxonomy_bloom IS NULL OR rtti IS NULL LIMIT ? OFFSET ?',
             (10, offset)).fetchall()
         return result
 
     def show_filtered_questions(self, subject, indexed, offset, search_term = ""):
         if indexed == 'indexed':
-            result = self.cursor.execute('SELECT * FROM questions WHERE question LIKE ? AND subject = ? AND taxonomy_bloom IS NOT NULL AND rtti IS NOT NULL LIMIT ? OFFSET ?',
+            result = self.cursor.execute('SELECT DISTINCT  * FROM questions WHERE question LIKE ? AND subject = ? AND taxonomy_bloom IS NOT NULL AND rtti IS NOT NULL LIMIT ? OFFSET ?',
                                          ("%"+search_term+"%", subject, 10, offset)).fetchall()
         elif indexed == 'not-indexed':
-            result = self.cursor.execute('SELECT * FROM questions WHERE question LIKE ? AND subject = ? AND taxonomy_bloom IS NULL OR rtti IS NULL LIMIT ? OFFSET ?',
+            result = self.cursor.execute('SELECT DISTINCT  * FROM questions WHERE question LIKE ? AND subject = ? AND taxonomy_bloom IS NULL OR rtti IS NULL LIMIT ? OFFSET ?',
                 ("%" + search_term + "%", subject, 10, offset)).fetchall()
         else:
-            result = self.cursor.execute('SELECT * FROM questions WHERE question LIKE ? AND subject = ? LIMIT ? OFFSET ?',
+            result = self.cursor.execute('SELECT DISTINCT  * FROM questions WHERE question LIKE ? AND subject = ? LIMIT ? OFFSET ?',
                                          ("%" + search_term + "%", subject, 10, offset)).fetchall()
         return result
 
