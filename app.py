9845791f8538bf75a38c4dc4785e3dae069647cc
yorick-1101625 <1101625@hr.nl>
2024-12-24 19:05:38 +0100

    Changed prompt overview to include the prompt category

--- app.py
+++ app.py
@@ -96,7 +96,6 @@ def prompt_create():
         prompts_model = Prompts()
         user_id = session.get('user_id')
         prompt_category = request.form.get("prompt_category")
-        print(prompt_category)
         created_prompt = prompts_model.create_prompt(prompt_name, prompt, user_id, prompt_category)
 
         if created_prompt:
